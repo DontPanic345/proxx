9ac677126476c84bc60e9221349dbe2eb85d30ac
Alan Falloon <alan@end-game.com>
2019-07-18 08:54:42 +1200

    change title to Sophie Sweeper

--- src/main/services/preact-canvas/components/top-bar/index.tsx
+++ src/main/services/preact-canvas/components/top-bar/index.tsx
@@ -116,7 +116,7 @@ export default class TopBar extends Component<Props, State> {
   }: Props) {
     return (
       <div class={topBar} role="banner">
-        <h1 class={title}>Proxx</h1>
+        <h1 class={title}>Sophie Sweeper</h1>
         <div class={squaresLeft} aria-label={gameStatusText(playMode)}>
           <Square class={squareIcon} />{" "}
           {toReveal!.toString().padStart(toRevealTotal!.toString().length, "0")}
